95540307b3d7dcb75c30099d0b4dc7fd8694ecc2
Abhigyan1997 <alokabhigyan65@gmail.com>
2023-04-25 16:09:53 +0530

    CRUD with mongoDB

--- app.js
+++ app.js
@@ -1,6 +1,46 @@
 const express = require('express');
 const mongoConnect=require('./util/database').mongoConnect;
+
 const app = express();
+const path = require('path');
+
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+//const Product = require('./models/product');
+// const User = require('./models/user');
+// const Cart = require('./models/cart');
+// const CartItem = require('./models/cart-item');
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  // User.findById(1)
+  //   .then(user => {
+  //     req.user = user;
+      next();
+  //   })
+  //   .catch(err => console.log(err));
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+// Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+// User.hasMany(Product);
+// User.hasOne(Cart);
+// Cart.belongsTo(User);
+// Cart.belongsToMany(Product, { through: CartItem });
+// Product.belongsToMany(Cart, { through: CartItem });
 
 mongoConnect(()=>{
     app.listen(3000);
