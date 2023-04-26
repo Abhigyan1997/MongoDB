483fd8749a63717619249a889f184421f665048d
Abhigyan1997 <alokabhigyan65@gmail.com>
2023-04-26 17:23:46 +0530

    Deleting Cart Item

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
-const express = require('express');
-const mongoConnect=require('./util/database').mongoConnect;
-
-const app = express();
 const path = require('path');
 
+const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-//const Product = require('./models/product');
+const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
-// const Cart = require('./models/cart');
-// const CartItem = require('./models/cart-item');
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -25,7 +22,7 @@ app.use((req, res, next) => {
   User.findById('6447b2c03bc5060ea12dd462')
     .then(user => {
       req.user = new User(user.name, user.email, user.cart, user._id);
-     next();
+      next();
     })
     .catch(err => console.log(err));
 });
@@ -35,14 +32,6 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-// Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
-// User.hasMany(Product);
-// User.hasOne(Cart);
-// Cart.belongsTo(User);
-// Cart.belongsToMany(Product, { through: CartItem });
-// Product.belongsToMany(Cart, { through: CartItem });
-
-mongoConnect(()=>{
-    app.listen(3000);
+mongoConnect(() => {
+  app.listen(3000);
 });
-    
