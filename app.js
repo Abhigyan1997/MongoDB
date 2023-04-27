932ba3a03bf9c565cc6cb98fdde0c57e777d3da7
Abhigyan1997 <alokabhigyan65@gmail.com>
2023-04-27 16:32:10 +0530

    Getting Order Placed

--- app.js
+++ app.js
@@ -35,3 +35,4 @@ app.use(errorController.get404);
 mongoConnect(() => {
   app.listen(3000);
 });
+
--- controllers/admin.js
+++ controllers/admin.js
@@ -1,6 +1,4 @@
 const Product = require('../models/product');
-const mongodb=require('mongodb');
-const ObjectId=mongodb.ObjectId;
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
@@ -15,11 +13,18 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product=new Product(title,price,description,imageUrl,null,req.user._id);
+  const product = new Product(
+    title,
+    price,
+    description,
+    imageUrl,
+    null,
+    req.user._id
+  );
   product
-  .save()
+    .save()
     .then(result => {
-     console.log(result);
+      // console.log(result);
       console.log('Created Product');
       res.redirect('/admin/products');
     })
@@ -34,10 +39,9 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
- Product.findById(prodId)
+  Product.findById(prodId)
     // Product.findById(prodId)
     .then(product => {
-     
       if (!product) {
         return res.redirect('/');
       }
@@ -57,10 +61,17 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  
-   const product=new Product(updatedTitle,updatedPrice,updatedDesc,updatedImageUrl,new ObjectId(prodId));
-   product.save() 
-   .then(result => {
+
+  const product = new Product(
+    updatedTitle,
+    updatedPrice,
+    updatedDesc,
+    updatedImageUrl,
+    prodId
+  );
+  product
+    .save()
+    .then(result => {
       console.log('UPDATED PRODUCT!');
       res.redirect('/admin/products');
     })
