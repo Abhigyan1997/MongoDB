7dc0054b66cb47dc84a0d113ee2f613be50e26eb
Abhigyan1997 <alokabhigyan65@gmail.com>
2023-04-28 16:48:02 +0530

    Edit&Delete Mongoose

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const path = require('path');
-
+const mongoose=require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+ 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+//const mongoConnect = require('./util/database').mongoConnect;
+//const User = require('./models/user');
 
 const app = express();
 
@@ -18,21 +19,29 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('6447b2c03bc5060ea12dd462')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('6447b2c03bc5060ea12dd462')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
+// mongoConnect(() => {
+//   app.listen(3000);
+// });
+ 
+mongoose
+.connect('mongodb+srv://alokabhigyan65:Abhi1997$$@sharpenerproject.msds32f.mongodb.net/?retryWrites=true&w=majority')
+.then(result=>{
   app.listen(3000);
-});
-
+})
+.catch(err=>{
+  console.log(err);
+})
