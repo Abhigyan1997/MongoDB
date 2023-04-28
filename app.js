c32f47be2ee1404838b91cb34b814057a51ee3ec
Abhigyan1997 <alokabhigyan65@gmail.com>
2023-04-28 17:36:22 +0530

    User Table Relation

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
  
 const errorController = require('./controllers/error');
 //const mongoConnect = require('./util/database').mongoConnect;
-//const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -19,14 +19,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('6447b2c03bc5060ea12dd462')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('644bb13a16e15a3477ef2edb')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -40,6 +40,18 @@ app.use(errorController.get404);
 mongoose
 .connect('mongodb+srv://alokabhigyan65:Abhi1997$$@sharpenerproject.msds32f.mongodb.net/?retryWrites=true&w=majority')
 .then(result=>{
+  User.findOne().then(user=>{
+    if(!user){
+      const user=new User({
+        name:'Alok',
+        email:'alokabhigyan65@gmail.com',
+        cart:{
+          items:[]
+        }
+      });
+      user.save();
+    }
+  })
   app.listen(3000);
 })
 .catch(err=>{
